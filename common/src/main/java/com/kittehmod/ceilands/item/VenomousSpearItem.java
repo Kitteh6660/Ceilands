3406e1366b61daf141032444943612258f155d2a
Kitteh6660 <Kitteh6660@users.noreply.github.com>
2024-12-25 03:27:58 -0500

    Fix Venomous Spear

--- common/src/main/java/com/kittehmod/ceilands/item/VenomousSpearItem.java
+++ common/src/main/java/com/kittehmod/ceilands/item/VenomousSpearItem.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.Nullable;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableMultimap.Builder;
 import com.google.common.collect.Multimap;
+import com.kittehmod.ceilands.util.MobEffectHelper;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -33,7 +34,7 @@ public class VenomousSpearItem extends Item implements Vanishable
 	public VenomousSpearItem(Properties properties) {
 		super(properties);
 		Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-	    builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", 6.0D, AttributeModifier.Operation.ADDITION));
+	    builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", 5.0D, AttributeModifier.Operation.ADDITION));
 	    builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Tool modifier", (double)-2.8F, AttributeModifier.Operation.ADDITION));
 		this.defaultModifiers = builder.build();
 	}
@@ -41,11 +42,11 @@ public class VenomousSpearItem extends Item implements Vanishable
 	@Override
 	public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 		if (!target.hasEffect(MobEffects.POISON)) {
-			target.addEffect(SPEAR_VENOM);
+			target.addEffect(MobEffectHelper.copyEffect(SPEAR_VENOM));
 		}
 		else if (target.getEffect(MobEffects.POISON).getDuration() < 160) {
 			target.removeEffect(MobEffects.POISON);
-			target.addEffect(SPEAR_VENOM);	
+			target.addEffect(MobEffectHelper.copyEffect(SPEAR_VENOM));	
 		}
 		stack.hurtAndBreak(1, attacker, (le) -> {
 			le.broadcastBreakEvent(EquipmentSlot.MAINHAND);
